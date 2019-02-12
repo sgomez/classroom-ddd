205bcc65be7939b385a6115501939a2bb12847ca
Sergio Gómez <sergio@uco.es>
2019-02-12 06:37:23 +0100

    Domain Driven Design skeleton

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,16 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    AulaSoftwareLibre\DDD\BaseBundle\BaseBundle::class => ['all' => true],
+    AulaSoftwareLibre\DDD\DevBundle\DevBundle::class => ['dev' => true, 'test' => true],
+    AulaSoftwareLibre\DDD\TestsBundle\TestBundle::class => ['test' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Prooph\Bundle\EventStore\ProophEventStoreBundle::class => ['all' => true],
 ];
